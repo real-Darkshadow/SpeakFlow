9b59ce731c0e59c0180a3f6e4914591509f4f214
Aadarsh Kumawat <kumawataadarsh1@gmail.com>
2023-05-21 11:37:36 +0530

    Google Sign in and Email Sign in Implemented
    Signed-off-by: Aadarsh Kumawat <kumawataadarsh1@gmail.com>

---
+++ app/src/main/java/com/app/speak/ui/fragments/LoginFragment.kt
@@ -0,0 +1,142 @@
+package com.app.speak.ui.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.app.speak.MainActivity
+import com.app.speak.R
+import com.app.speak.databinding.FragmentLoginBinding
+import com.app.speak.db.AppPrefManager
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
+
+class LoginFragment : Fragment() {
+    var _binding: FragmentLoginBinding? = null
+    val appPrefManager by lazy { AppPrefManager(requireActivity()) }
+    val binding get() = _binding!!
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    val RC_SIGN_IN: Int = 1
+    lateinit var gso: GoogleSignInOptions
+    lateinit var mAuth: FirebaseAuth
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+        mAuth = FirebaseAuth.getInstance()
+        createRequest()
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.apply {
+            googleSignin.setOnClickListener {
+                GoogleSignIn()
+            }
+            registerButton.setOnClickListener {
+                findNavController().navigate(R.id.registerFragment)
+            }
+            signinCard.setOnClickListener {
+                EmailSignIn()
+            }
+        }
+
+    }
+
+    private fun EmailSignIn() {
+        val email = binding.userEmail.text.toString()
+        val password = binding.userPassword.text.toString()
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "createUserWithEmail:success")
+                    val user = mAuth.currentUser
+                    appPrefManager.setUserData(user?.email.toString(), user?.email.toString())
+                    startActivity(Intent(requireActivity(), MainActivity::class.java))
+                    requireActivity().finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        requireContext(),
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+    }
+
+    private fun createRequest() {
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.your_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+    }
+
+    private fun GoogleSignIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    @Suppress
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                firebaseAuthWithGoogle(account)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(
+                    requireContext(),
+                    "Login Failed: ${e.statusCode}",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+                Log.e("GoogleSignIn", "Sign-in failed", e)
+                // ...
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val user = mAuth.currentUser
+                    appPrefManager.setUserData(user?.displayName.toString(), user?.email.toString())
+                    startActivity(Intent(requireActivity(), MainActivity::class.java))
+                    requireActivity().finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(requireActivity(), "Login Failed: ", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
--- app/src/main/java/com/app/speak/ui/fragments/RegisterFragment.kt
+++ app/src/main/java/com/app/speak/ui/fragments/RegisterFragment.kt
@@ -1,25 +1,80 @@
 package com.app.speak.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.app.speak.MainActivity
 import com.app.speak.R
+import com.app.speak.databinding.FragmentRegisterBinding
+import com.app.speak.db.AppPrefManager
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 
 
 class RegisterFragment : Fragment() {
+    var _binding: FragmentRegisterBinding? = null
+    val appPrefManager by lazy { AppPrefManager(requireActivity()) }
+    val binding get() = _binding!!
+    lateinit var gso: GoogleSignInOptions
+    lateinit var mAuth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        _binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
+        mAuth = FirebaseAuth.getInstance()
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        binding.apply {
+            signinCard.setOnClickListener {
+                signIn()
+            }
+            loginButton.setOnClickListener {
+                findNavController().navigate(R.id.loginFragment)
+            }
+        }
 
     }
+
+    private fun signIn() {
+        val userName = binding.userName.text.toString()
+        val email = binding.userEmail.text.toString()
+        val password = binding.userPassword.text.toString()
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "createUserWithEmail:success")
+                    val user = mAuth.currentUser
+                    appPrefManager.setUserData(userName, user?.email.toString())
+                    startActivity(Intent(requireActivity(), MainActivity::class.java))
+                    requireActivity().finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        requireContext(),
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                }
+            }
+    }
+
 
 }

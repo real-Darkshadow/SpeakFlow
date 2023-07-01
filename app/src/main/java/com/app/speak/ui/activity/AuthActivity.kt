60d6561bcf2bc317ee3d529c191e3724a177700a
Aadarsh <kumawataadarsh1@gmail.com>
2023-07-01 15:54:29 +0530

    login and register ui

--- app/src/main/java/com/app/speak/ui/activity/AuthActivity.kt
+++ app/src/main/java/com/app/speak/ui/activity/AuthActivity.kt
@@ -15,6 +15,7 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        changeStatusBarColor(color = R.color.white,0)
     }
 
     override fun onStart() {

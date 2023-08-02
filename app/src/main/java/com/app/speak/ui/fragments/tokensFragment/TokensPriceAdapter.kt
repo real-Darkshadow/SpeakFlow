7b65612950f2ae12be12e1255bfa41240411fdc9
Aadarsh <kumawataadarsh1@gmail.com>
2023-08-02 16:04:26 +0530

    plan ui change and other ui optimization and download functionality

--- app/src/main/java/com/app/speak/ui/fragments/tokensFragment/TokensPriceAdapter.kt
+++ app/src/main/java/com/app/speak/ui/fragments/tokensFragment/TokensPriceAdapter.kt
@@ -1,56 +1,69 @@
 package com.app.speak.ui.fragments.tokensFragment
 
+import ExtensionFunction.gone
+import ExtensionFunction.visible
+import android.annotation.SuppressLint
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.RadioButton
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.app.speak.R
+import com.app.speak.databinding.TokenPriceOptionsBinding
 import com.app.speak.models.PlanPrices
 
-class TokensPriceAdapter(val planPrices: List<PlanPrices>, val onclick: (String) -> Unit) :
+class TokensPriceAdapter(
+    private val context: Context,
+    val planPrices: List<PlanPrices>, val onclick: (String) -> Unit
+) :
     RecyclerView.Adapter<TokensPriceAdapter.ViewHolder>() {
-    private var selectedItem = -1
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.token_price_options, parent, false)
-        return ViewHolder(view)
+        val binding =
+            TokenPriceOptionsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
         return planPrices.size
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val planPrice = planPrices[holder.adapterPosition]
-
-        holder.title.text = planPrice.planName
-        holder.des.text = planPrice.planPrice
-
-        holder.layout.setOnClickListener {
-            val clickedPosition = holder.adapterPosition
-            if (clickedPosition != RecyclerView.NO_POSITION && selectedItem != clickedPosition) {
-                if (selectedItem != RecyclerView.NO_POSITION) {
-                    planPrices[selectedItem].isSelected = false
-                    notifyItemChanged(selectedItem)
-                }
-                planPrice.isSelected = true
-                selectedItem = clickedPosition
-                onclick(planPrice.id)
-                notifyItemChanged(clickedPosition)
+        with(holder) {
+            binding.pricing.text = "$" + planPrices[position].planPrice
+            binding.planName.text = planPrices[position].planName
+            binding.characterPerMont.text = planPrices[position].characters + "characters per month"
+            if (planPrices[position].recommended == true) binding.recommendedChip.visible()
+            else binding.recommendedChip.gone()
+            binding.monthCard.setOnClickListener {
+                onclick(planPrices[position].id)
+                val isSelected = true // Set isSelected based on your business logic
+                display(isSelected)
             }
         }
 
-        holder.radioButton.isChecked = planPrice.isSelected
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val title: TextView = itemView.findViewById(R.id.add_more)
-        val des: TextView = itemView.findViewById(R.id.des)
-        val layout: LinearLayout = itemView.findViewById(R.id.layout)
-        val radioButton: RadioButton = itemView.findViewById(R.id.radioButton)
+    inner class ViewHolder(val binding: TokenPriceOptionsBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun display(isSelected: Boolean) {
+            binding.monthCard.background =
+                if (isSelected)
+                    ContextCompat.getDrawable(
+                        context,
+                        R.drawable.tokens_card_gradient
+                    )
+                else
+                    ContextCompat.getDrawable(
+                        context,
+                        R.drawable.tokens_card_gradient
+                    )
+        }
     }
 }

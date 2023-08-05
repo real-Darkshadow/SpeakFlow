3c06c32c5e963cd37b9250db2f1e63850a356041
Aadarsh <kumawataadarsh1@gmail.com>
2023-08-05 19:58:56 +0530

    code optimization

--- app/src/main/java/com/app/speak/ui/history/DashboardFragment.kt
+++ app/src/main/java/com/app/speak/ui/history/DashboardFragment.kt
@@ -66,7 +66,7 @@ class DashboardFragment : Fragment() {
                         when (bool) {
                             false -> {
                                 viewModel.regeneratePrompt.value = string
-                                findNavController().clearBackStack(R.id.navigation_home)
+                                findNavController().navigate(R.id.navigation_home)
                             }
 
                             true -> {
